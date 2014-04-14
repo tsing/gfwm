d941d83d8105713e365afaea3fccb54cf1fb6de5
tsing <me@tsing.org>
2014-04-14 23:16:31 +0800

    Add conflict detector

--- test/store.js
+++ test/store.js
@@ -20,3 +20,22 @@ describe('Store.load', function() {
     ret.value.should.be.eql(['google.com']);
   });
 });
+
+describe('Store.isContained', function() {
+  var store = new Store('abc.json');
+
+  it('conflict when subdomain is in domains', function() {
+    var conflicted = store.isConflict(['www.google.com'], 'google.com');
+    conflicted.should.be.eql(['www.google.com', 'google.com']);
+  });
+
+  it('should return [] when nothing conflict', function() {
+    var conflicted = store.isConflict(['www.google.com'], 'www.google.cn');
+    conflicted.should.be.eql([]);
+  });
+
+  it('conflict when subdomain is provided', function() {
+    var conflicted = store.isConflict(['google.com'], 'www.google.com');
+    conflicted.should.be.eql(['google.com', 'www.google.com']);
+  });
+});
